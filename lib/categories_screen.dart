42dcd27db25def5848fe43f1e15131402d3a6cdc
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-06-07 09:32:17 -0400

    Added the ability to click the category and land on another page

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'dummy_data.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[
-        
-      ],
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3/2,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Deli Meals'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((catData) =>
+                CategoryItem(catData.id, catData.title, catData.color))
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,35 +11,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Deli Meals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.white,
+        canvasColor: const Color.fromRGBO(0, 0, 0, 0.9),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: const TextStyle(
+            color: Color.fromRGBO(255, 255, 255, 1),
+          ),
+          bodyText2: const TextStyle(
+            color: Color.fromRGBO(255, 255, 255, 1),
+          ),
+          headline1: const TextStyle(
+            fontSize: 22,
+            fontFamily: 'RobotoCondensed',
+            color: Color.fromRGBO(255, 255, 255, 1),
+            fontWeight: FontWeight.bold,
+          ),
+        )
       ),
-      home: MyHomePage(),
+      home: CategoryScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body: const Center(
-        child: Text('NAVIGATION TIME!'),
-      ),
-    );
-  }
-}
 
